b57b910166fb909cf438d9f15bced16e68c106e1
WANG,Youtian <wangyoutian@msn.com>
2014-10-22 23:10:06 +0800

    lib ini

---
+++ lib/double_/Eq.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nilnul.num.real.double_
+{
+	public partial class Eq
+	{
+		static public bool NoMoreThan1_1000000_(double x , double y) {
+			return Math.Abs(x - y) <= 0.000001;
+		}
+	}
+}
